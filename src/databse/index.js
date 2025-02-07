6cf965615138b2c6fdbd05593e830192e05d25fe
m2cci-boudinl <lina.boudin@live.fr>
2025-02-07 15:01:35 +0100

    Supprime les options inutiles de connexion a la base

--- src/databse/index.js
+++ src/databse/index.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const configOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
 const connectToDB = async () => {
-    const connectUrl = process.env.MONGODB_URI;
-    mongoose.connect(connectUrl, configOptions)
-    .then(() => console.log('DataBase connected successfully'))
-    .catch((err) => console.log(`Getting error from DB connection ${err.message}`))
-}
+  const connectUrl = process.env.MONGODB_URI;
+  mongoose
+    .connect(connectUrl)
+    .then(() => console.log("DataBase connected successfully"))
+    .catch((err) =>
+      console.log(`Getting error from DB connection ${err.message}`)
+    );
+};
 
 export default connectToDB;
