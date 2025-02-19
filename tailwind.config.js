64d8727b4355d4a55b534f5f397d6db3d5d5a4e7
m2cci-boudinl <lina.boudin@live.fr>
2025-02-19 16:26:51 +0100

    Corrige traitement des styles dynamiques tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        bordeaux: "#6a0d1a", // Bordeaux
-        button: "#222623",
-        noir: "#000000", // Noir
-        blanc: "#FFFFFF", // Blanc
-        or: "#cfb53b", // Or
-      },
-      fontFamily: {
-        bookAntiqua: ['"Book Antiqua"', "serif"], // Ajoute Book Antiqua comme police
-      },
-      fontSize: {
-        base: "1.125rem", // Taille de texte par défaut (augmentée de 1rem à 1.125rem)
-      },
+export const content = [
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      background: "var(--background)",
+      foreground: "var(--foreground)",
+      bordeaux: "#6a0d1a", // Bordeaux
+      button: "#222623",
+      noir: "#000000", // Noir
+      blanc: "#FFFFFF", // Blanc
+      or: "#cfb53b", // Or
+    },
+    fontFamily: {
+      bookAntiqua: ['"Book Antiqua"', "serif"], // Ajoute Book Antiqua comme police
+    },
+    fontSize: {
+      base: "1.125rem", // Taille de texte par défaut (augmentée de 1rem à 1.125rem)
     },
   },
-  plugins: [],
 };
+export const plugins = [];
