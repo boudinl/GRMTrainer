67af1f3252726bb1eb414e53257ba312a314108d
m2cci-boudinl <lina.boudin@live.fr>
2025-03-05 10:11:44 +0100

    Modifie les styles

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -59,8 +59,8 @@ export default function Orders() {
 
   return (
     <section>
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="mt-8 mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto  sm:px-6 lg:px-8">
+        <div className="mt-8 mx-auto max-w-screen-xl  sm:px-6 lg:px-8">
           <div>
             <div className="px-4 py-6 sm:px-8 sm:py-10">
               <div className="flow-root">
@@ -71,19 +71,20 @@ export default function Orders() {
                         key={item._id}
                         className="bg-white shadow p-5 flex flex-col space-y-3 py-6 text-left"
                       >
-                        <div className="flex">
-                          <h1 className="font-bold text-lg mb-3 flex-1">
+                        <div className="flex flex-col sm:flex-row">
+                          <h1 className="font-bold text-sm sm:text-lg mb-3 sm:mb-0 sm:mr-3 flex-1">
                             #commande: {item._id}
                           </h1>
                           <div className="flex items-center">
-                            <p className="mr-3 text-sm font-medium text-gray-900">
+                            <p className="m-1 sm:mr-3 text-sm font-medium text-gray-900">
                               Montant total payé
                             </p>
-                            <p className="mr-3 text-2xl  font-semibold text-gray-900">
+                            <p className="sm:mr-3 text-sm md:text-2xl font-semibold text-gray-900">
                               {item.totalPrice}€
                             </p>
                           </div>
                         </div>
+
                         <div className="flex gap-2">
                           {item.orderItems.map((orderItem, index) => (
                             <div key={index} className="shrink-0">
@@ -100,7 +101,7 @@ export default function Orders() {
                           ))}
                         </div>
                         <div className="flex gap-5">
-                          <button className="disabled:opacity-50 mt-5 mr-5  inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide">
+                          <button className="disabled:opacity-50 mt-5 mr-5  inline-block bg-black text-white p-2 sm:px-5 sm:py-3 text-xs font-medium uppercase tracking-wide">
                             {item.isProcessing
                               ? "Commande en cours"
                               : "Commande reçu"}
